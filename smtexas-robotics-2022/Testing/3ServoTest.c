c76b910760ba03540ea2061059ec5efcc69e13d4
Arav-R <63694299+Arav-R@users.noreply.github.com>
2022-11-14 18:39:01 -0600

    Finished 3 servo test

--- smtexas-robotics-2022/Testing/3ServoTest.c
+++ smtexas-robotics-2022/Testing/3ServoTest.c
@@ -19,11 +19,47 @@ int servo3Far = 0;
 
 task main()
 {
-	while true {
+	while (true) {
 
 
-	}
+		if (vexRT[Btn6D] == 1) {
+			motor[servo1] = servo1Far; // Servo 1 Far position - Lift Down
+
+		} else if (vexRT[Btn6U] == 1) {
+			motor[servo1] = servo1Close; // Servo 1 Close position - Lift Up
+
+		} else {
+			motor[servo1] = servo1Middle; // Servo 1 Middle position - Lift Stop
+		}
+
+
+		if (vexRT[Ch3] > 25) {
+			motor[servo2] = servo2Far; // Servo 2 Far position - Drive Forward
+
+		} else if (vexRT[Ch3] < -25) {
+			motor[servo2] = servo2Close; // Servo 2 Close position - Drive Backwards
+
+		} else {
+			motor[servo2] = servo2Middle; // Servo 2 Middle position - Drive Stop
+		}
+
+
+		if (vexRT[Ch1] > 25) {
+			motor[servo3] = servo3Far; // Servo 3 Far position - Rotate Right
+
+		} else if (vexRT[Ch1] < -25) {
+			motor[servo3] = servo3Close; // Servo 3 Close position - Rotate Left
+
+		} else {
+			motor[servo3] = servo3Middle; // Servo 3 Middle position - Rotate Stop
+		}
 
 
 
-}
+
+
+	} //while end
+
+
+
+} //main end

a9b6a77a69f1b91c883b12a45d994915c1e96e07
Arav-R <63694299+Arav-R@users.noreply.github.com>
2022-11-30 00:43:53 -0600

    added arm slow mode

--- smtexas-robotics-2022/armJoy.h
+++ smtexas-robotics-2022/armJoy.h
@@ -10,12 +10,23 @@ Non-Generic Code
 float armPos = 0; //default position
 int armHigh = -100; //low position
 int armLow = -15; //high position
+float slowMulti = 1;
+
 // control motor to drop light pole
 void arm_joy(Controller *c, int arm_port1) {
-		if (vexRT[Ch2] >= 45) {
-  			armPos -= .02;	// move up
+
+	if (c->btn5.up) {
+		slowMulti = 0.25;
+	} else {
+		slowMulti = 1;
+	}
+
+
+
+	if (vexRT[Ch2] >= 45) {
+  			armPos -= .05 * slowMulti;	// move up
   	} else if (vexRT[Ch2] <= -45) {
-  			armPos += .02;	// move down
+  			armPos += .05 * slowMulti;	// move down
   	}
 
   	if (armPos < armHigh){
